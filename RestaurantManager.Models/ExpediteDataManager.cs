5721780a9f5a987aa0f4de6d5d03435d7cf6d193
PabloOrazi <pabloorazi@gmail.com>
2016-03-13 21:27:40 -0300

    Assignment 1

---
+++ RestaurantManager.Models/ExpediteDataManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace RestaurantManager.Models
+{
+    public class ExpediteDataManager : DataManager
+    {
+        protected override void OnDataLoaded()
+        {
+            
+        }
+
+        private List<Order> _orderItems;
+
+        public List<Order> OrderItems
+        {
+            get { return base.Repository.Orders; }
+            set
+            {
+                if (value != _orderItems)
+                {
+                    _orderItems = value;
+                    OnPropertyChanged();
+
+                }
+            }
+        }
+    }
+}
---
+++ RestaurantManager.Models/OrderDataManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace RestaurantManager.Models
+{
+    public class OrderDataManager : DataManager
+    {
+        protected override void OnDataLoaded()
+        {
+            this.MenuItems = base.Repository.StandardMenuItems;
+
+            this.CurrentlySelectedMenuItems = new List<MenuItem>
+            {
+                this.MenuItems[3],
+                this.MenuItems[5]
+            };
+        }
+
+        private List<MenuItem> _menuItems;
+
+        public List<MenuItem> MenuItems
+        {
+            get { return _menuItems; }
+            set
+            {
+                if ( value != _menuItems)
+                {
+                    _menuItems = value;
+                    this.OnPropertyChanged(); 
+                }
+            }
+        }
+
+        private List<MenuItem> _currentlySelectedMenuItems;
+        public List<MenuItem> CurrentlySelectedMenuItems
+        {
+            get
+            {
+                return _currentlySelectedMenuItems;
+            }
+            set
+            {
+                if (value != _currentlySelectedMenuItems)
+                {
+                    _currentlySelectedMenuItems = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+    }
+}
