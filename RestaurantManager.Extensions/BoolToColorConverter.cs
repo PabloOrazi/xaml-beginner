2ea44464cb238505078680b83457dea41110e9fb
PabloOrazi <pabloorazi@gmail.com>
2016-03-25 12:37:22 -0300

    Assignment 3

---
+++ RestaurantManager.Extensions/BoolToColorConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace RestaurantManager.Extensions
+{
+    public class BoolToColorConverter : IValueConverter
+    {
+        public Color TrueColor { get; set; }
+
+        public Color FalseColor { get; set; }
+                
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return (value != null && value is bool && (bool)value == true) ? TrueColor: FalseColor;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ RestaurantManager.Extensions/RightClickMessageDialogBehavior.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Xaml.Interactivity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace RestaurantManager.Extensions
+{
+    public class RightClickMessageDialogBehavior : DependencyObject, IBehavior
+    {
+        public string Message { get; set; }
+        public string Title { get; set; }
+
+
+        public DependencyObject AssociatedObject
+        {
+            get;
+            private set;
+        }
+
+        public void Attach(DependencyObject associatedObject)
+        {
+            this.AssociatedObject = associatedObject;
+            (this.AssociatedObject as Page).RightTapped += Right_Button_Click;
+        }
+
+        public void Detach()
+        {
+            (this.AssociatedObject as Page).RightTapped -= Right_Button_Click;
+        }
+
+        private async void Right_Button_Click(object sender, RoutedEventArgs e)
+        {
+            await new MessageDialog(this.Message, this.Title).ShowAsync();
+        }
+    }
+}
